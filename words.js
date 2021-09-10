d2cecdcd0ebbeb85fb3bce486b5bd73c6806a3ab
Hidde Wieringa <hidde@hiddewieringa.nl>
2021-09-10 16:44:11 +0200

    index and words

---
+++ words.js
@@ -0,0 +1,18 @@
+var words = [
+	// Normal
+	'STUDIE',
+	'KOEKEN',
+	'ABACUS',
+	'BOEKEN',
+	'ACTIEF',
+	// 10 letter word
+	'VERENIGING',
+	// Normal
+	'STUDENT',
+	'IDEAAL',
+	'TAIPAN',
+	'LETTER',
+	'ZILVER',
+	// 10 letter word
+	'BESTUURSDAS',
+];
